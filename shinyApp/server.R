14adf3485b703cd273df946c3912dc4b969687b1
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 13:43:13 -0600

    update

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -2,16 +2,14 @@ shinyServer(function(input, output) {
 
   
     source('https://raw.githubusercontent.com/bolus123/PH1ANDPH2App/master/shinyApp/functions.R', local = TRUE)
-  
-    #aabb <- function(x) x * 10
-  
+
   
     output$plot1 <- renderPlot({
         Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
         Ph2Data <- rnorm(input$Ph2testBatches * input$Ph2testSampleSize, input$Ph2testNormMu, sqrt(input$Ph2testNormSigma2))
         
-        Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testBatches, nrow = input$Ph1testSampleSize)
-        Ph1Data <- matrix(Ph2Data, ncol = input$Ph2testBatches, nrow = input$Ph2testSampleSize)
+        Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
+        Ph2Data <- matrix(Ph2Data, ncol = input$Ph2testSampleSize, nrow = input$Ph2testBatches)
         
         Ph1Obj <- Ph1ChartStatAndPars(Ph1Data)
         
