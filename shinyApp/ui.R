0b12743f7b1dc9c77a7add9e6fb381b63e1f182f
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 09:42:12 -0600

    update

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
   
   # Application title
   
-    titlePanel("Statistical Performance Analysis"),
+    titlePanel("Phase I and Phase I Control Charts"),
   
   
   
@@ -24,13 +24,26 @@ shinyUI(fluidPage(
             fileInput('Ph1data', 'Choose Phase I Data',
                 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
                 
-            textInput("caption", "Caption", "Data Summary"),
+                
+            #######################################################################################    
+            textInput("Ph1testNormMu", "Ph1 Test parameters mu", "0"),
+            textInput("Ph1testNormSigma2", "Ph1 Test parameters sigma2", "1"),
+            textInput("Ph1testBatches", "Ph1 Test parameters batches", "10"),
+            textInput("Ph1testSampleSize", "Ph1 Test parameters sample size", "5"),
+            #######################################################################################
       
             selectInput("Ph2cc", "Phase II Control Chart:", 
                   choices = c("Basic Phase II X-bar Chart")),
       
             fileInput('Ph2data', 'Choose Phase II Data',
-                accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+                accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')), 
+            
+            #######################################################################################            
+            textInput("Ph2testNormMu", "Ph2 Test parameters mu", "0"),      
+            textInput("Ph2testNormSigma2", "Ph2 Test parameters sigma2", "1"),
+            textInput("Ph2testBatches", "Ph2 Test parameters batches", "10"),
+            textInput("Ph2testSampleSize", "Ph2 Test parameters sample size", "5")
+            #######################################################################################
       
         ),
     
