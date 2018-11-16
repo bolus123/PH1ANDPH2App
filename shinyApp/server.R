5390ebcc4029014036f9ea749307efb6c4e18b90
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 09:50:31 -0600

    update

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,61 +1,60 @@
-
-ControlChartPlot <- function(ChartStat, LowerLimit, UpperLimit) {
-
-    m <- length(ChartStat)
-    
-    if (length(LowerLimit) == 1) {
-    
-        LowerLimit.vec <- rep(LowerLimit, m)
-    
-    } else {
-    
-        LowerLimit.vec <- LowerLimit
-    
-    }
-    
-    if (length(UpperLimit) == 1) {
-    
-        UpperLimit.vec <- rep(UpperLimit, m)
-    
-    } else {
-    
-        UpperLimit.vec <- UpperLimit
-    
-    }
-    
-    plot.max <- max(c(ChartStat, LowerLimit.vec, UpperLimit.vec))
-    plot.min <- min(c(ChartStat, LowerLimit.vec, UpperLimit.vec))
-    
-    ####This part will be replaced by ggplot2
-    
-    plot(c(1, m), c(plot.min, plot.max), type = 'n', xlab = 'Batches', ylab = 'Charting Statistics')
-    points(seq(1, m, 1), ChartStat, type = 'o', lty = 1)
-    points(seq(1, m, 1), LowerLimit.vec, type = 'l', lty = 2)
-    points(seq(1, m, 1), UpperLimit.vec, type = 'l', lty = 2)
-
-}
-
-#############################################################################
-
-
 shinyServer(function(input, output) {
+  
+    #########################################################################
+    
+    ControlChartPlot <- function(ChartStat, LowerLimit, UpperLimit) {
+
+        m <- length(ChartStat)
+        
+        if (length(LowerLimit) == 1) {
+        
+            LowerLimit.vec <- rep(LowerLimit, m)
+        
+        } else {
+        
+            LowerLimit.vec <- LowerLimit
+        
+        }
+        
+        if (length(UpperLimit) == 1) {
+        
+            UpperLimit.vec <- rep(UpperLimit, m)
+        
+        } else {
+        
+            UpperLimit.vec <- UpperLimit
+        
+        }
+        
+        plot.max <- max(c(ChartStat, LowerLimit.vec, UpperLimit.vec))
+        plot.min <- min(c(ChartStat, LowerLimit.vec, UpperLimit.vec))
+        
+        ####This part will be replaced by ggplot2
+        
+        plot(c(1, m), c(plot.min, plot.max), type = 'n', xlab = 'Batches', ylab = 'Charting Statistics')
+        points(seq(1, m, 1), ChartStat, type = 'o', lty = 1)
+        points(seq(1, m, 1), LowerLimit.vec, type = 'l', lty = 2)
+        points(seq(1, m, 1), UpperLimit.vec, type = 'l', lty = 2)
+
+    }
+  
   
     #########################################################################
     ###Test Phase 1 data
     
-        Ph1Data <- matrix(
-                rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2)), 
-                ncol = input$Ph1testSampleSize,
-                nrow = input$Ph1testBatches
-            )
-            
-        Ph1ChartStat <- rowMeans(Ph1Data)
+    Ph1Data <- matrix(
+            rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2)), 
+            ncol = input$Ph1testSampleSize,
+            nrow = input$Ph1testBatches
+        )
         
-        Ph1mu <- mean(Ph1ChartStat)
-        Ph1sigma <- sqrt(mean(diag(var(t(Ph1Data)))))
-        
-        Ph1LowerLimit <- Ph1mu - 3 * Ph1sigma
-        Ph1UpperLimit <- Ph1mu + 3 * Ph1sigma
+    Ph1ChartStat <- rowMeans(Ph1Data)
+    
+    Ph1mu <- mean(Ph1ChartStat)
+    Ph1sigma <- sqrt(mean(diag(var(t(Ph1Data)))))
+    
+    Ph1LowerLimit <- Ph1mu - 3 * Ph1sigma
+    Ph1UpperLimit <- Ph1mu + 3 * Ph1sigma
     
     #########################################################################
   
