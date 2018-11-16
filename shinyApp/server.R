de5458a29361c6b02f86038254c3634a8a0215f8
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 16:02:54 -0600

    github

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -60,11 +60,13 @@ shinyServer(function(input, output) {
         Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
         tb <- Ph1Statistics(Ph1Data)
         
-        data.frame(
-            Metric = unlist(tb$Metric),
-            Value = unlist(tb$Value),
-            stringsAsFactors = FALSE
-        )
+        #data.frame(
+        #    Metric = unlist(tb$Metric),
+        #    Value = unlist(tb$Value),
+        #    stringsAsFactors = FALSE
+        #)
+
+        c(length(tb$Metric), length(tb$Value))
         
         #tb <- cbind(tb$Metric, round(tb$Value, 4))
         #colnames(tb) <- c('Metric', 'Value')
