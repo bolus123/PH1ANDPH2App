84823b1bb7f1b29213dbdc20d7e255f3e9534bd6
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-15 20:21:27 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,31 +2,47 @@ require(shiny)
 require(shinythemes)
 
 # Define UI for Statistical Performance Analysis (spa)
-shinyUI(fluidPage(
+
+ui <- shinyUI(fluidPage(
+  
   # Application theme
+  
   theme = shinytheme("flatly"),
   
+  
+  
   # Application title
-  titlePanel("Phase I and Phase II Control Charts"),
-
+  
+  titlePanel("Statistical Performance Analysis"),
+  
+  
+  
   sidebarLayout(
+    
     sidebarPanel(
+      
       selectInput("Ph1cc", "Phase I Control Chart:", 
                   choices = c("Basic Phase I X-bar Chart")),
-
+      
       fileInput('Ph1data', 'Choose Phase I Data',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'),
+                accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
       
       selectInput("Ph2cc", "Phase II Control Chart:", 
                   choices = c("Basic Phase II X-bar Chart")),
-
+      
       fileInput('Ph2data', 'Choose Phase II Data',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'),
+                accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+      
     ),
     
-
+    
+    
+    
+    
     mainPanel(
+      
       tabsetPanel(type = "tabs", 
+                  
                   tabPanel("Exploration",
                            fluidRow(
                              column(5,
@@ -39,45 +55,50 @@ shinyUI(fluidPage(
                              )
                            )
                   ),
-
+                  
                   tabPanel("Monitoring",
                            fluidRow(
                              column(6,
-                                #plotOutput("cdf"), plotOutput("pdf")
-                                textOutput("text3")
+                                    #plotOutput("cdf"), plotOutput("pdf")
+                                    textOutput("text3")
                              ),
                              column(6,
-                                #plotOutput("cdfrl1"), plotOutput("pdfrl1"))
-                                textOutput("text4")
+                                    #plotOutput("cdfrl1"), plotOutput("pdfrl1"))
+                                    textOutput("text4")
                              )
-                                    
+                             
+                           )
                   ),
                   
                   tabPanel("Performance",
                            fluidRow(
                              column(7,
-                                #plotOutput("cdf"), plotOutput("pdf")
-                                textOutput("text5")
+                                    #plotOutput("cdf"), plotOutput("pdf")
+                                    textOutput("text5")
                              ),
                              column(7,
-                                #plotOutput("cdfrl1"), plotOutput("pdfrl1"))
-                                textOutput("text6")
+                                    #plotOutput("cdfrl1"), plotOutput("pdfrl1"))
+                                    textOutput("text6")
                              )     
+                           )
                   )
                   
+                  
+                  
       )
+      
     )
-  )
-))
-
-  
-server <- shinyServer(function(input, output) {
-  
-  output$test <- renderPlot({
-    x    <- rnorm(100)
-    hist(x, col = 'darkgray', border = 'white')
-  })
-})
-
-  
-shinyApp(ui = ui, server = server)
+    
+  )))
+      
+      
+      server <- shinyServer(function(input, output) {
+        
+        output$test <- renderPlot({
+          x    <- rnorm(100)
+          hist(x, col = 'darkgray', border = 'white')
+        })
+      })
+      
+      
+      shinyApp(ui = ui, server = server)
