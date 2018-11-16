8695ec6f3161a4ab0fdcf047db8f8b053064433b
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 10:43:24 -0600

    update

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -7,9 +7,9 @@ shinyServer(function(input, output) {
   
   
     output$plot1 <- renderPlot({
-        x <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
+        Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
         
-        Ph1Obj <- Ph1ChartStatAndLimits(X)
+        Ph1Obj <- Ph1ChartStatAndLimits(Ph1Data)
         #x <- rnorm(100)
         #x <- aabb(x)
         hist(Ph1Obj$Ph1ChartStat, col = 'darkgray', border = 'white')
