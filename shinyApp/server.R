a390624b3d6a7b435859afc7e72de00a50d50979
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 16:17:05 -0600

    update

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -3,15 +3,27 @@ shinyServer(function(input, output) {
   
     source('https://raw.githubusercontent.com/bolus123/PH1ANDPH2App/master/shinyApp/functions.R', local = TRUE)
 
+    
+################################################################################################################ 
+
+    Ph1Data <- reactive({
+        Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
+        Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
+        Ph1Data
+    })
+    
+    Ph2Data <- reactive({
+        Ph2Data <- rnorm(input$Ph2testBatches * input$Ph2testSampleSize, input$Ph2testNormMu, sqrt(input$Ph2testNormSigma2))
+        Ph2Data <- matrix(Ph2Data, ncol = input$Ph2testSampleSize, nrow = input$Ph2testBatches)
+        Ph2Data
+    })
+    
   
 ################################################################################################################ 
   
     output$plot1 <- renderPlot({
-        Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
-        Ph2Data <- rnorm(input$Ph2testBatches * input$Ph2testSampleSize, input$Ph2testNormMu, sqrt(input$Ph2testNormSigma2))
-        
-        Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
-        Ph2Data <- matrix(Ph2Data, ncol = input$Ph2testSampleSize, nrow = input$Ph2testBatches)
+        Ph1Data <- Ph1Data()
+        Ph2Data <- Ph2Data()
         
         Ph1Obj <- Ph1ChartStatAndPars(Ph1Data)
         
@@ -57,9 +69,8 @@ shinyServer(function(input, output) {
     
     
     output$table1 <- renderTable({
-        Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
-        Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
-        tb <- Ph1Statistics(Ph1Data)
+
+        tb <- Ph1Statistics(Ph1Data())
         
         data.frame(
             Metric = unlist(tb$Metric),
