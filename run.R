17f2b9c7e3e854248e34a427529b304218334d0d
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-16 09:20:50 -0600

    update

--- run.R
+++ run.R
@@ -1,1 +1,16 @@
-shinyApp(ui = ui, server = server)
+check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+}
+
+
+###########################################################################################
+
+packages<-c("shiny", "shinythemes")
+check.packages(packages)
+
+###########################################################################################
+
+runGitHub('bolus123/PH1ANDPH2App', subdir = 'shinyApp')
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,18 +1,3 @@
-check.packages <- function(pkg){
-    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-    if (length(new.pkg)) 
-        install.packages(new.pkg, dependencies = TRUE)
-    sapply(pkg, require, character.only = TRUE)
-}
-
-
-###########################################################################################
-
-packages<-c("shiny", "shinythemes")
-check.packages(packages)
-
-###########################################################################################
-
 # Define UI for Statistical Performance Analysis (spa)
 
 shinyUI(fluidPage(
