bb594738981895d7fc6c67421c89cbf560431f5d
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-19 15:42:06 -0600

    update

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -6,26 +6,42 @@ shinyServer(function(input, output) {
     
 ################################################################################################################ 
 
-    Ph1Data <- reactive({
-        req(input$Ph1Data)
-        inFile <- input$Ph1Data
-        as.matrix(read_excel(inFile$datapath, 1))
-    })
+    #Ph1Data <- reactive({
+    #    req(input$Ph1Data)
+    #    inFile <- input$Ph1Data
+    #    as.matrix(read_excel(inFile$datapath, 1))
+    #})
 
     Ph1Dimension <- reactive({
         dim(Ph1Data())
     })
     
-    #Ph1Data <- reactive({
-    #    Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
-    #    Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
-    #    Ph1Data
-    #})
+    Ph1Data <- reactive({
+        if (input$Ph1cc == 'Basic Ph I Testing') {
+            Ph1Data <- rnorm(input$Ph1testBatches * input$Ph1testSampleSize, input$Ph1testNormMu, sqrt(input$Ph1testNormSigma2))
+            Ph1Data <- matrix(Ph1Data, ncol = input$Ph1testSampleSize, nrow = input$Ph1testBatches)
+        } else {
+            req(input$Ph1Data)
+            inFile <- input$Ph1Data
+            Ph1Data <- as.matrix(read_excel(inFile$datapath, 1))
+        }
+        
+        Ph1Data
+        
+    })
     
     Ph2Data <- reactive({
-        Ph2Data <- rnorm(input$Ph2testBatches * input$Ph2testSampleSize, input$Ph2testNormMu, sqrt(input$Ph2testNormSigma2))
-        Ph2Data <- matrix(Ph2Data, ncol = input$Ph2testSampleSize, nrow = input$Ph2testBatches)
+        if (input$Ph2cc == 'Basic Ph I Testing') {
+            Ph2Data <- rnorm(input$Ph2testBatches * input$Ph2testSampleSize, input$Ph2testNormMu, sqrt(input$Ph2testNormSigma2))
+            Ph2Data <- matrix(Ph2Data, ncol = input$Ph2testSampleSize, nrow = input$Ph2testBatches)
+        } else {
+            req(input$Ph2Data)
+            inFile <- input$Ph2Data
+            Ph2Data <- as.matrix(read_excel(inFile$datapath, 1))
+        }
+        
         Ph2Data
+        
     })
  
 ################################################################################################################
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -31,10 +31,13 @@ shinyUI(fluidPage(
                     numericInput("FAP", "Ph1 False Alarm Probability", 0.05, min = 0, max = 1)
                 ),
                 
-                #numericInput("Ph1testNormMu", "Ph1 Test parameters mu", 0),
-                #numericInput("Ph1testNormSigma2", "Ph1 Test parameters sigma2", 1, min = 1e-6),
-                #numericInput("Ph1testBatches", "Ph1 Test parameters batches", 10, min = 1),
-                #numericInput("Ph1testSampleSize", "Ph1 Test parameters sample size", 5, min = 2),
+                conditionalPanel(
+                    condition = "input.Ph1cc == 'Basic Ph I Testing'",
+                    numericInput("Ph1testNormMu", "Ph1 Test parameters mu", 0),
+                    numericInput("Ph1testNormSigma2", "Ph1 Test parameters sigma2", 1, min = 1e-6),
+                    numericInput("Ph1testBatches", "Ph1 Test parameters batches", 10, min = 1),
+                    numericInput("Ph1testSampleSize", "Ph1 Test parameters sample size", 5, min = 2)
+                ),
               
                 #######################################################################################
           
@@ -43,14 +46,30 @@ shinyUI(fluidPage(
                 #        choices = c("Basic Phase II X-bar Chart")),
           
                 fileInput('Ph2Data', 'Choose Phase II Data',
-                    accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')), 
+                    accept = c(".xlsx")),
                 
-                #######################################################################################            
-                numericInput("Ph2testNormMu", "Ph2 Test parameters mu", 0),      
-                numericInput("Ph2testNormSigma2", "Ph2 Test parameters sigma2", 1, min = 1e-6),
-                numericInput("Ph2testBatches", "Ph2 Test parameters batches", 10, min = 1),
-                numericInput("Ph2testSampleSize", "Ph2 Test parameters sample size", 5, min = 2),
-                numericInput("Ph2testCC", "Ph2 Test Charting constant", 3, min = 1e-6)
+                ####################################################################################### 
+                conditionalPanel(
+                    condition = "input.Ph2cc == 'Basic Phase II X-bar Chart(UC)'",
+                    numericInput("ARL0", "Ph2 In-control Average Run Length", 370, min = 1)
+                ),                
+                
+                conditionalPanel(
+                    condition = "input.Ph2cc == 'Basic Phase II X-bar Chart(EPC)'",
+                    numericInput("Alpha", "Alpha", 0.05, min = 1e-6, max = 1),
+                    numericInput("Eps", "Eps", 0.1, min = 0),
+                    numericInput("P", "P", 0.05, min = 1e-6, max = 1)
+                ),  
+                
+                conditionalPanel(
+                    condition = "input.Ph2cc == 'Basic Ph II Testing'",
+                    numericInput("Ph2testNormMu", "Ph2 Test parameters mu", 0),      
+                    numericInput("Ph2testNormSigma2", "Ph2 Test parameters sigma2", 1, min = 1e-6),
+                    numericInput("Ph2testBatches", "Ph2 Test parameters batches", 10, min = 1),
+                    numericInput("Ph2testSampleSize", "Ph2 Test parameters sample size", 5, min = 2),
+                    numericInput("Ph2testCC", "Ph2 Test Charting constant", 3, min = 1e-6)
+                )
+                
                 #######################################################################################
           
             ),
