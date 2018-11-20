ecfcc591f91d9e6a0df7031aa296b6976463da0a
Yuhui Yao <bolus123@users.noreply.github.com>
2018-11-20 14:50:34 -0600

    update

--- run.R
+++ run.R
@@ -13,4 +13,8 @@ check.packages(packages)
 
 ###########################################################################################
 
-runGitHub('bolus123/PH1ANDPH2App', subdir = 'shinyApp')
+options(shiny.port = 38383)
+
+options(shiny.host = "127.0.0.1")
+
+runGitHub('bolus123/PH1ANDPH2App', subdir = 'shinyApp', host = getOption("shiny.host", "127.0.0.1"))
